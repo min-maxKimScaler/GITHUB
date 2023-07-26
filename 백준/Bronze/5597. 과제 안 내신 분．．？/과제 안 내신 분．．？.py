ce3d2d4261627a5d8881e7ee4ae3e086f5c1d10f
kimstar-GitHub <109965570+min-maxKimScaler@users.noreply.github.com>
2023-07-27 00:07:44 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,14 +1,1 @@
-st=[]
-for i in range(30):
-    a = 0
-    a += i+1
-    st.append(a)
-for i in range(28):
-    st_num=int(input())
-    if st_num in st:
-        st.remove(st_num)
-    else:
-        st_num=0
-sorted(st)
-for i in st:
-    print(i)
+print(*{*map(int,open(0))}^{*range(1,31)})
