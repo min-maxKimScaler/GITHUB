a1256286dd25ad2abbde9bf516100e66f742e4df
kimstar-GitHub <109965570+min-maxKimScaler@users.noreply.github.com>
2023-07-27 09:03:24 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,7 @@
+s=input()
+alphabet=['a','b','c','d','e','f','g','h','i',
+          'j','k','l','m','n','o','p','q','r',
+          's','t','u','v','w','x','y','z']
+
+for char in alphabet:
+    print(s.find(char),end=" ")
